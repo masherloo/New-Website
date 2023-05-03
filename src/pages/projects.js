410982a1019c3b19ad6c62aecdee0dec0cf019de
Mohammadreza Asherloo <masherloo@hawk.iit.edu>
2023-05-03 12:38:09 -0500

    fixed navigation bar and the eductaion section

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -123,7 +123,7 @@ const projects = () => {
       <TransitionEffect />
       <main className="w-full mb-16 flex flex-col items-center justify-center">
         <Layout className="pt-16">
-          <AmimatedText text="Creativity conquers wisdom!" className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8" />
+          <AmimatedText text="Creativity means everything!" className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8" />
           <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
             <div className="col-span-12">
               <FeaturedProject
