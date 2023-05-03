af0660474efd309104e5609c342820e5eb4270a3
Mohammadreza Asherloo <masherloo@hawk.iit.edu>
2023-05-02 20:22:54 -0500

    fixed the z-index issue

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,27 +8,25 @@ export const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
 
   return (
-    <Link href={href}>
-      <motion.a className={`${className} relative group`}>
-        {title}
-        <span
-          className={`h-[3px] inline-block w-0 absolute
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`h-[3px] inline-block w-0 absolute
        left-0 -bottom-0.5 bg-black group-hover:w-full
         transition-[width] ease duration-300
         ${router.pathname === href ? "w-full" : "w-0"}
         `}
-        >
-          &nbsp;
-        </span>
-        <span
-          className={`h-0 inline-block w-[3px] absolute
+      >
+        &nbsp;
+      </span>
+      <span
+        className={`h-0 inline-block w-[3px] absolute
        -left-1 bottom-0 bg-black group-hover:h-full
         transition-[height] ease duration-300
         ${router.pathname === href ? "h-full" : "h-0"}`}
-        >
-          &nbsp;
-        </span>
-      </motion.a>
+      >
+        &nbsp;
+      </span>
     </Link>
   );
 };
@@ -37,34 +35,34 @@ export const CustomMobileLink = ({ href, title, className = "", toggle }) => {
   const router = useRouter();
 
   const handleClick = () => {
+    toggle();
     router.push(href);
   };
 
   return (
-    <Link href={href} passHref>
-      <motion.a
-        className={`${className} relative group text-light my-2`}
-        onClick={handleClick}
-      >
-        {title}
-        <span
-          className={`h-[3px] inline-block w-0 absolute
+    <Link
+      href={href}
+      className={`${className} relative group text-light my-2`}
+      onClick={handleClick}
+    >
+      {title}
+      <span
+        className={`h-[3px] inline-block w-0 absolute
        left-0 -bottom-0.5 bg-light group-hover:w-full
         transition-[width] ease duration-300
         ${router.pathname === href ? "w-full" : "w-0"}
         `}
-        >
-          &nbsp;
-        </span>
-        <span
-          className={`h-0 inline-block w-[3px] absolute
+      >
+        &nbsp;
+      </span>
+      <span
+        className={`h-0 inline-block w-[3px] absolute
        -left-1 bottom-0 bg-light group-hover:h-full
         transition-[height] ease duration-300
         ${router.pathname === href ? "h-full" : "h-0"}`}
-        >
-          &nbsp;
-        </span>
-      </motion.a>
+      >
+        &nbsp;
+      </span>
     </Link>
   );
 };
@@ -79,7 +77,7 @@ const NavBar = () => {
   return (
     <header
       className="w-full px-32 py-8 font-medium flex items-center
-     justify-between relative z-10 lg:px-16 md:px-12 sm:px-8"
+     justify-between relative z-50 lg:px-16 md:px-12 sm:px-8"
     >
       <button
         className="flex-col justify-center items-center hidden lg:flex"
@@ -119,7 +117,7 @@ const NavBar = () => {
           initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
           animate={{ scale: 1, opacity: 1 }}
           className="min-w-[40vw] flex flex-col justify-between items-center
-          fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 bg-dark/90 rounded-lg backdrop-blur-md py-32"
+          fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 rounded-lg backdrop-blur-md py-32 z-50"
         >
           <nav className="flex items-center flex-col justify-center">
             <CustomMobileLink
